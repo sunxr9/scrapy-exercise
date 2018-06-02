e5cc2aacdd445d5d80bb4a4880fd149ba8ae8e8c
sunxr9 <sunxiaoran9@163.com>
2018-06-02 16:43:48 +0800

    生成models文件，ython manage.py inspectdb > web/models.py

---
+++ web/web/models.py
@@ -0,0 +1,74 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Comments(models.Model):
+    commentid = models.IntegerField(primary_key=True)
+    pid = models.BigIntegerField()
+    cid = models.BigIntegerField()
+    avatar = models.CharField(max_length=512, blank=True, null=True)
+    uname = models.CharField(max_length=512, blank=True, null=True)
+    created_at = models.CharField(max_length=128)
+    content = models.TextField(blank=True, null=True)
+    like_counts = models.IntegerField()
+    reply = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'comments'
+
+
+class Composers(models.Model):
+    cid = models.BigIntegerField(primary_key=True)
+    banner = models.CharField(max_length=512)
+    avatar = models.CharField(max_length=512)
+    verified = models.IntegerField()
+    name = models.CharField(max_length=128)
+    intro = models.TextField(blank=True, null=True)
+    like_counts = models.IntegerField()
+    fans_counts = models.IntegerField()
+    follow_counts = models.IntegerField()
+    location = models.CharField(max_length=32, blank=True, null=True)
+    career = models.CharField(max_length=32, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'composers'
+
+
+class Copyrights(models.Model):
+    pcid = models.CharField(primary_key=True, max_length=32)
+    pid = models.BigIntegerField()
+    cid = models.BigIntegerField()
+    roles = models.CharField(max_length=32)
+
+    class Meta:
+        managed = False
+        db_table = 'copyrights'
+
+
+class Posts(models.Model):
+    pid = models.BigIntegerField(primary_key=True)
+    title = models.CharField(max_length=256)
+    thumbnail = models.CharField(max_length=512, blank=True, null=True)
+    preview = models.CharField(max_length=512, blank=True, null=True)
+    video = models.CharField(max_length=512, blank=True, null=True)
+    video_format = models.CharField(max_length=32, blank=True, null=True)
+    duration = models.IntegerField()
+    category = models.CharField(max_length=512)
+    created_at = models.CharField(max_length=128)
+    description = models.TextField(blank=True, null=True)
+    play_counts = models.IntegerField()
+    like_counts = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'posts'
